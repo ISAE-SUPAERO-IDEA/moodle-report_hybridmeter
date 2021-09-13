20d46146ec7564e76d448d3e7c12fbcc1cf8d65c
Nassim Bennouar <Nassim.BENNOUAR@isae-supaero.fr>
2021-09-13 14:05:09 +0200

    fix generaldata

--- hybridmetrics/classes/traitement.php
+++ hybridmetrics/classes/traitement.php
@@ -82,8 +82,7 @@ class traitement{
 		$generaldata['nb_cours_hybrides_statiques']=count($generaldata['cours_hybrides_statiques']);
 		$generaldata['nb_cours_hybrides_dynamiques']=count($generaldata['cours_hybrides_dynamiques']);
 
-		$generaldata['etudiants_concernes_statiques']=$this->data->count_distinct_students($generaldata['id_hybrides_statiques']);
-		$generaldata['nb_etudiants_concernes_statiques']=count($generaldata['etudiants_concernes_statiques']);
+		$generaldata['nb_etudiants_concernes_statiques']=$this->data->count_distinct_students($generaldata['id_hybrides_statiques']);
 
 		$generaldata['nb_etudiants_concernes_statiques_actifs']=$this->data->count_single_users_course_viewed($generaldata['id_hybrides_statiques'],$this->configurator->get_begin_timestamp(),$this->configurator->get_end_timestamp());
 		
