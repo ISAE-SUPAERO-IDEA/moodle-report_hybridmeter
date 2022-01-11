78e7ee37a2a4db2fab82a4c1ad6e9b0e1c94ed7f
Nassim Bennouar <Nassim.BENNOUAR@isae-supaero.fr>
2022-01-11 15:49:30 +0100

    scheduled task disabled

--- hybridmeter/db/tasks.php
+++ hybridmeter/db/tasks.php
@@ -1,14 +1,4 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
-$tasks = array(  
-     array(
-          'classname' => 'report_hybridmeter\task\traitement_regulier', 
-          'blocking' => 0,
-          'minute' => '0',
-          'hour' => '0',
-          'day' => '*',
-          'dayofweek' => '*', 
-          'month' => '*'
-     )
-);
+$tasks = array();
 ?>
