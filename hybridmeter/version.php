d1b8e3bdb1b4062ca8199bab1aa6999c73ee5daf
Nassim Bennouar <Nassim.BENNOUAR@isae-supaero.fr>
2022-06-23 15:33:49 +0200

    CI tools updated : semantic changed and env file feature

--- hybridmeter/version.php
+++ hybridmeter/version.php
@@ -5,5 +5,3 @@ $plugin->version = 2022042100;
 $plugin->requires = 2018051700; // Moodle 3.2.9 minimum
 $plugin->release = "1.1.3";
 $plugin->maturity = MATURITY_BETA;
-
-//PHP 7.1 required, which means Moodle 3.2 !!!
