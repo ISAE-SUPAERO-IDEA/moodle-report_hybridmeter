4a04e3c0457162818f8e744007464fda6b5f89ea
Nassim Bennouar <Nassim.BENNOUAR@isae-supaero.fr>
2022-09-23 11:04:56 +0200

    version number updated

--- hybridmeter/version.php
+++ hybridmeter/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $plugin->component = "report_hybridmeter";
-$plugin->version = 2022092207;
+$plugin->version = 2022092300;
 $plugin->requires = 2018051700; // Moodle 3.5.7 minimum
 $plugin->release = "1.1.3";
 $plugin->maturity = MATURITY_BETA;
