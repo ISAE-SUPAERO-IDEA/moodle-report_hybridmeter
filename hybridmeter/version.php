2763737be945084f94cfea093778a35f2b9390ac
John Tranier <john.tranier@isae-supaero.fr>
2024-01-25 11:29:40 +0100

    Set plugin maturity to "Release Candidate"

--- hybridmeter/version.php
+++ hybridmeter/version.php
@@ -21,4 +21,4 @@ $plugin->component = "report_hybridmeter";
 $plugin->version = 2023103100;
 $plugin->requires = 2018051700; // Moodle 3.5.7 minimum
 $plugin->release = "1.1.3";
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_RC;
