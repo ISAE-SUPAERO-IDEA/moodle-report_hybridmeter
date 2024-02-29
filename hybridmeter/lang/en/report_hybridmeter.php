a91ab4718fcb6e4860281782f5e63ef2c6576995
John Tranier <john.tranier@isae-supaero.fr>
2024-02-29 15:58:45 +0100

    Update copyrights

--- hybridmeter/lang/en/report_hybridmeter.php
+++ hybridmeter/lang/en/report_hybridmeter.php
@@ -1,22 +1,24 @@
 <?php
-/*
- * Hybrid Meter
- * Copyright (C) 2020 - 2024  ISAE-SUPAERO
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// This file is part of Moodle - http://moodle.org
+//
+//  Moodle is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Moodle is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * @author Nassim Bennouar, Bruno Ilponse
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright (C) 2020  ISAE-SUPAERO (https://www.isae-supaero.fr/)
+ */
 require_once(__DIR__."/../../constants.php");
 
 // Settings
