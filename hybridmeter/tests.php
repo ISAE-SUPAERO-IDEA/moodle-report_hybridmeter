3ba59f319cd98f3f8116daff5563bf64d54024ef
John Tranier <john.tranier@isae-supaero.fr>
2024-03-04 10:53:36 +0100

    Fix PHPCS error on tests.php

--- hybridmeter/tests.php
+++ hybridmeter/tests.php
@@ -19,17 +19,18 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @copyright (C) 2020  ISAE-SUPAERO (https://www.isae-supaero.fr/)
  */
-require(dirname(__FILE__).'/../../config.php');
-require(dirname(__FILE__).'/classes/test_context.php');
-require(dirname(__FILE__).'/classes/tests/NU/inconsistent_nu.php');
-require(dirname(__FILE__).'/classes/tests/ND/inconsistent_nd.php');
-require(dirname(__FILE__).'/classes/tests/course_count/inconsistent_registered_active_students.php');
-require(dirname(__FILE__).'/classes/tests/blacklist_scenario.php');
+require(dirname(__FILE__) . '/../../config.php');
+require(dirname(__FILE__) . '/classes/test_context.php');
+require(dirname(__FILE__) . '/classes/tests/NU/inconsistent_nu.php');
+require(dirname(__FILE__) . '/classes/tests/ND/inconsistent_nd.php');
+require(dirname(__FILE__) . '/classes/tests/course_count/inconsistent_registered_active_students.php');
+require(dirname(__FILE__) . '/classes/tests/blacklist_scenario.php');
 
-use \report_hybridmeter\classes\tests\ND\inconsistent_nd as inconsistent_nd;
-use \report_hybridmeter\classes\tests\NU\inconsistent_nu as inconsistent_nu;
-use \report_hybridmeter\classes\tests\course_count\inconsistent_registered_active_students as inconsistent_registered_active_students;
-use \report_hybridmeter\classes\tests\blacklist_scenario as blacklist_scenario;
+use report_hybridmeter\classes\tests\ND\inconsistent_nd as inconsistent_nd;
+use report_hybridmeter\classes\tests\NU\inconsistent_nu as inconsistent_nu;
+use report_hybridmeter\classes\tests\course_count\inconsistent_registered_active_students
+    as inconsistent_registered_active_students;
+use report_hybridmeter\classes\tests\blacklist_scenario as blacklist_scenario;
 
 require_login();
 $context = context_system::instance();
@@ -37,38 +38,38 @@ $PAGE->set_context($context);
 has_capability('report/hybridmeter:all', $context) || die();
 
 
-$retour_management = "<p><a href=\"management.php\">Retour à l'écran de configuration</a></p>";
-echo $retour_management;
+$backlink = "<p><a href=\"management.php\">Retour à l'écran de configuration</a></p>";
+echo $backlink;
 
-$tests_set = array();
+$testsset = [];
 
 $task = required_param('task', PARAM_TEXT);
 
-switch($task) {
-    case "blacklist" :
-        array_push($tests_set, new blacklist_scenario());
+switch ($task) {
+    case "blacklist":
+        array_push($testsset, new blacklist_scenario());
         break;
 
-    case "course" :
+    case "course":
         $id = required_param('id', PARAM_INT);
 
         global $DB;
 
-        $course_info = $DB->get_record('course', array("id" => $id));
+        $courseinfo = $DB->get_record('course', ["id" => $id]);
 
-        echo "<h1>Cours n°".$id." : ".$course_info->fullname."</h1>";
+        echo "<h1>Cours n°" . $id . " : " . $courseinfo->fullname . "</h1>";
 
-        array_push($tests_set, new inconsistent_nd($id));
-        array_push($tests_set, new inconsistent_nu($id));
-        array_push($tests_set, new inconsistent_registered_active_students($id));
+        array_push($testsset, new inconsistent_nd($id));
+        array_push($testsset, new inconsistent_nu($id));
+        array_push($testsset, new inconsistent_registered_active_students($id));
 
         break;
 
-    default :
+    default:
         echo "<p>Paramètre task incorrect</p>";
         break;
 }
 
-\report_hybridmeter\classes\test_context::launch_batch($tests_set);
+\report_hybridmeter\classes\test_context::launch_batch($testsset);
 
-echo $retour_management;
+echo $backlink;
