c1be01deac34819c8eb3c87e898396936a14feca
John Tranier <john.tranier@isae-supaero.fr>
2024-03-04 10:56:07 +0100

    Fix PHPCS error on planned_tasks.php

--- hybridmeter/planned_tasks.php
+++ hybridmeter/planned_tasks.php
@@ -22,13 +22,13 @@
 require(dirname(__FILE__).'/../../config.php');
 require_once('classes/data_provider.php');
 
-use \report_hybridmeter\classes\data_provider as data_provider;
+use report_hybridmeter\classes\data_provider as data_provider;
 
 require_login();
 $context = context_system::instance();
 $PAGE->set_context($context);
 has_capability('report/hybridmeter:all', $context) || die();
 
-$planned_tasks = data_provider::get_instance()->get_adhoc_tasks_list();
+$plannedtasks = data_provider::get_instance()->get_adhoc_tasks_list();
 
-echo json_encode($planned_tasks);
+echo json_encode($plannedtasks);
