67c724e02e8cdb2367879a1b66fc93ed75cc02c1
John Tranier <john.tranier@isae-supaero.fr>
2024-03-04 11:15:48 +0100

    Fix PHPCS error on renderer.php

--- hybridmeter/output/renderer.php
+++ hybridmeter/output/renderer.php
@@ -19,12 +19,13 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @copyright (C) 2020  ISAE-SUPAERO (https://www.isae-supaero.fr/)
  */
+
 namespace report_hybridmeter\output;
 
 defined('MOODLE_INTERNAL') || die;
 
-require_once(dirname(__FILE__)."/../../../config.php");
-require_once(dirname(__FILE__).'/../constants.php');
+require_once(dirname(__FILE__) . "/../../../config.php");
+require_once(dirname(__FILE__) . '/../constants.php');
 
 use plugin_renderer_base;
 use html_writer;
@@ -33,15 +34,15 @@ use DateTime;
 
 class renderer extends plugin_renderer_base {
 
-    public function next_schedule($is_scheduled, $timestamp_scheduled, $unscheduled_action) {
+    public function next_schedule($isscheduled, $timestampscheduled, $unscheduledaction) {
         $html = "";
 
-        $date_scheduled = new DateTime();
-        $date_scheduled->setTimestamp($timestamp_scheduled);
+        $datescheduled = new DateTime();
+        $datescheduled->setTimestamp($timestampscheduled);
 
-        if($unscheduled_action == 1){
+        if ($unscheduledaction == 1) {
             $html .= html_writer::start_div('container-fluid');
-            
+
             $html .= html_writer::span(get_string('successfully_unscheduled', 'report_hybridmeter'));
 
             $html .= html_writer::end_div();
@@ -49,15 +50,14 @@ class renderer extends plugin_renderer_base {
 
         $html .= html_writer::start_div('container-fluid');
 
-        if($is_scheduled == 1){
-            $schedule_message = sprintf(
+        if ($isscheduled == 1) {
+            $schedulemessage = sprintf(
                 get_string('next_schedule', 'report_hybridmeter'),
-                $date_scheduled->format('d/m/Y'),
-                $date_scheduled->format('H:i')
+                $datescheduled->format('d/m/Y'),
+                $datescheduled->format('H:i')
             );
-            $html .= html_writer::span($schedule_message, '');
-        }
-        else{
+            $html .= html_writer::span($schedulemessage, '');
+        } else {
             $html .= html_writer::span(get_string('no_schedule', 'report_hybridmeter'));
         }
 
@@ -71,14 +71,14 @@ class renderer extends plugin_renderer_base {
         );
         $html .= html_writer::start_span('');
 
-        $url_schedule = new moodle_url('/report/hybridmeter/management.php#schedule');
-        $url_unschedule = new moodle_url('/report/hybridmeter/index.php?unschedule=1');
+        $urlschedule = new moodle_url('/report/hybridmeter/management.php#schedule');
+        $urlunschedule = new moodle_url('/report/hybridmeter/index.php?unschedule=1');
 
-        if($is_scheduled == 1){
+        if ($isscheduled == 1) {
             $html .= html_writer::start_span('');
 
             $html .= html_writer::link(
-                $url_schedule,
+                $urlschedule,
                 get_string('reschedule', 'report_hybridmeter'),
                 array(
                     'class' => 'row m-1 btn btn-secondary',
@@ -91,7 +91,7 @@ class renderer extends plugin_renderer_base {
             $html .= html_writer::start_span('');
 
             $html .= html_writer::link(
-                $url_unschedule,
+                $urlunschedule,
                 get_string('unschedule', 'report_hybridmeter'),
                 array(
                     'class' => 'row m-1 btn btn-secondary',
@@ -100,10 +100,9 @@ class renderer extends plugin_renderer_base {
             );
 
             $html .= html_writer::end_span('');
-        }
-        else{
+        } else {
             $html .= html_writer::link(
-                $url_schedule,
+                $urlschedule,
                 get_string('schedule', 'report_hybridmeter'),
                 array(
                     'class' => 'row m-1 btn btn-secondary',
@@ -115,23 +114,23 @@ class renderer extends plugin_renderer_base {
         $html .= html_writer::end_span();
         $html .= html_writer::end_div();
 
-        $html .= html_writer::tag("hr","");
+        $html .= html_writer::tag("hr", "");
 
         return $html;
     }
 
-    public function index_links($is_data_available) {
+    public function index_links($isdataavailable) {
 
         // Download button
-        $html ="";
+        $html = "";
 
         $html .= html_writer::start_div('container-fluid');
-        $disabled = ($is_data_available) ? "" : "disabled";
-        $url = new moodle_url('/report/hybridmeter/index.php', array("task" => "download",));
+        $disabled = ($isdataavailable) ? "" : "disabled";
+        $url = new moodle_url('/report/hybridmeter/index.php', array("task" => "download"));
         $html .= html_writer::link(
             $url,
             get_string('download_csv', 'report_hybridmeter'),
-            array('class' => 'row m-1 btn btn-primary '.$disabled),
+            array('class' => 'row m-1 btn btn-primary ' . $disabled),
         );
         $html .= html_writer::end_div();
 
@@ -147,28 +146,25 @@ class renderer extends plugin_renderer_base {
             ));
         $html .= html_writer::end_div();
 
-
         // Documentation link
-        $html .= html_writer::tag("hr","");
+        $html .= html_writer::tag("hr", "");
 
         $url = 'https://doc.clickup.com/d/h/2f5v0-8317/29996805f942cfc';
         $html .= html_writer::link($url, get_string('documentation', 'report_hybridmeter'),
-            array('target' => 'blank',));
+            array('target' => 'blank'));
 
-        $html .= html_writer::tag("br","");
-            
+        $html .= html_writer::tag("br", "");
+
         // Changelog link
         $url = 'https://doc.clickup.com/d/h/2f5v0-8568/7b507d8c7c54778';
         $html .= html_writer::link($url, get_string('changelog', 'report_hybridmeter'),
-            array('target' => 'blank',));
-            
+            array('target' => 'blank'));
 
-        
         return $html;
     }
 
-    public function is_task_planned(int $count_pending, int $is_running){
-        $html = html_writer::tag("hr","");
+    public function is_task_planned(int $countpending, int $isrunning) {
+        $html = html_writer::tag("hr", "");
         $html .= html_writer::start_div('container-fluid');
 
         $url = new moodle_url('/report/hybridmeter/index.php',
@@ -178,7 +174,8 @@ class renderer extends plugin_renderer_base {
             )
         );
 
-        $html .= html_writer::link($url,
+        $html .= html_writer::link(
+            $url,
             "Clear tasks",
             array(
                 'class' => 'row m-1 btn btn-secondary',
@@ -186,13 +183,11 @@ class renderer extends plugin_renderer_base {
             )
         );
 
-        if($is_running != REPORT_HYBRIDMETER_NON_RUNNING){
+        if ($isrunning != REPORT_HYBRIDMETER_NON_RUNNING) {
             $html .= html_writer::span(get_string('task_running', 'report_hybridmeter'), '');
-        }
-        else if($count_pending > 0){
+        } else if ($countpending > 0) {
             $html .= html_writer::span(get_string('task_pending', 'report_hybridmeter'), '');
-        }
-        else{
+        } else {
             $html .= html_writer::span(get_string('no_task_pending', 'report_hybridmeter'), '');
         }
         $html .= html_writer::end_div();
@@ -200,7 +195,7 @@ class renderer extends plugin_renderer_base {
         return $html;
     }
 
-    public function last_calculation($is_data_available, $date, $interval){
+    public function last_calculation($isdataavailable, $date, $interval) {
 
         $html = html_writer::start_div('container-fluid');
 
@@ -210,7 +205,8 @@ class renderer extends plugin_renderer_base {
                 "debug" => 1,
             ));
 
-        $html .= html_writer::link($url,
+        $html .= html_writer::link(
+            $url,
             get_string('recalculate', 'report_hybridmeter'),
             array(
                 'class' => 'row m-1 btn btn-secondary',
@@ -218,118 +214,121 @@ class renderer extends plugin_renderer_base {
             )
         );
 
-        $date = ($is_data_available && isset($date)) ? $date : REPORT_HYBRIDMETER_NA;
+        $date = ($isdataavailable && isset($date)) ? $date : REPORT_HYBRIDMETER_NA;
         $content = sprintf(get_string('last_updated', 'report_hybridmeter'), $date, $interval);
 
         $html .= html_writer::span($content);
 
         $html .= html_writer::end_div();
 
-        $html .= html_writer::tag("hr","");
+        $html .= html_writer::tag("hr", "");
 
         return $html;
     }
 
-    public function general_indicators($is_data_available, $generaldata, $timestamp_begin, $timestamp_end, $end_processing, $processing_duration){
+    public function general_indicators($isdataavailable, $generaldata, $timestampbegin, $timestampend, $endprocessing, $processingduration) {
         global $OUTPUT;
 
+        $doesdataexist = ($isdataavailable && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_DIGITALISED_COURSES]));
+        $nbcourshybridesstatiques = $doesdataexist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_DIGITALISED_COURSES] : REPORT_HYBRIDMETER_NA;
 
-        $does_data_exist = ($is_data_available && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_DIGITALISED_COURSES]));
-        $nb_cours_hybrides_statiques = $does_data_exist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_DIGITALISED_COURSES] : REPORT_HYBRIDMETER_NA;
+        $doesdataexist = ($isdataavailable && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_USED_COURSES]));
+        $nbcourshybridesdynamiques = $doesdataexist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_USED_COURSES] : REPORT_HYBRIDMETER_NA;
 
-        $does_data_exist = ($is_data_available && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_USED_COURSES]));
-        $nb_cours_hybrides_dynamiques = $does_data_exist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_USED_COURSES] : REPORT_HYBRIDMETER_NA;
+        $doesdataexist = ($isdataavailable && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_DIGITALISED]));
+        $nbetudiantsconcernesstatiques = $doesdataexist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_DIGITALISED] : REPORT_HYBRIDMETER_NA;
 
-        $does_data_exist = ($is_data_available && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_DIGITALISED]));
-        $nb_etudiants_concernes_statiques = $does_data_exist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_DIGITALISED] : REPORT_HYBRIDMETER_NA;
+        $doesdataexist = ($isdataavailable && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_DIGITALISED_ACTIVE]));
+        $nbetudiantsconcernesstatiquesactifs = $doesdataexist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_DIGITALISED_ACTIVE] : REPORT_HYBRIDMETER_NA;
 
-        $does_data_exist = ($is_data_available && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_DIGITALISED_ACTIVE]));
-        $nb_etudiants_concernes_statiques_actifs = $does_data_exist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_DIGITALISED_ACTIVE] : REPORT_HYBRIDMETER_NA;
+        $doesdataexist = ($isdataavailable && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_USED]));
+        $nbetudiantsconcernesdynamiques = $doesdataexist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_USED] : REPORT_HYBRIDMETER_NA;
 
-        $does_data_exist = ($is_data_available && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_USED]));
-        $nb_etudiants_concernes_dynamiques = $does_data_exist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_USED] : REPORT_HYBRIDMETER_NA;
+        $doesdataexist = ($isdataavailable && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_USED_ACTIVE]));
+        $nbetudiantsconcernesdynamiquesactifs = $doesdataexist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_USED_ACTIVE] : REPORT_HYBRIDMETER_NA;
 
-        $does_data_exist = ($is_data_available && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_USED_ACTIVE]));
-        $nb_etudiants_concernes_dynamiques_actifs = $does_data_exist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_USED_ACTIVE] : REPORT_HYBRIDMETER_NA;
+        $doesdataexist = ($isdataavailable && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_ANALYSED_COURSES]));
+        $nbcoursanalyses = $doesdataexist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_ANALYSED_COURSES] : REPORT_HYBRIDMETER_NA;
 
-        $does_data_exist = ($is_data_available && isset($generaldata[REPORT_HYBRIDMETER_GENERAL_NB_ANALYSED_COURSES]));
-        $nb_cours_analyses = $does_data_exist ? $generaldata[REPORT_HYBRIDMETER_GENERAL_NB_ANALYSED_COURSES] : REPORT_HYBRIDMETER_NA;
-        
-        if($is_data_available && isset($timestamp_begin) && isset($timestamp_end)) {
-            $datetime_begin = new DateTime();
-            $datetime_end = new DateTime();
+        if ($isdataavailable && isset($timestampbegin) && isset($timestampend)) {
+            $datetimebegin = new DateTime();
+            $datetimeend = new DateTime();
 
-            $datetime_begin->setTimestamp($timestamp_begin);
-            $datetime_end->setTimestamp($timestamp_end);
+            $datetimebegin->setTimestamp($timestampbegin);
+            $datetimeend->setTimestamp($timestampend);
 
             $format = "d/m/Y";
 
-            $string_measurement_period = sprintf(
-                get_string('measurement_period','report_hybridmeter'),
-                $datetime_begin->format($format),
-                $datetime_end->format($format)
+            $stringmeasurementperiod = sprintf(
+                get_string('measurement_period', 'report_hybridmeter'),
+                $datetimebegin->format($format),
+                $datetimeend->format($format)
             );
-        }
-        else{
-            $string_measurement_period = REPORT_HYBRIDMETER_NA;
+        } else {
+            $stringmeasurementperiod = REPORT_HYBRIDMETER_NA;
         }
 
-        if($is_data_available && isset($end_processing)){
-            $string_end_processing = sprintf(
-                get_string('end_processing','report_hybridmeter'),
-                $end_processing
+        if ($isdataavailable && isset($endprocessing)) {
+            $stringendprocessing = sprintf(
+                get_string('end_processing', 'report_hybridmeter'),
+                $endprocessing
             );
-            $processing_date = date_parse_from_format('d/m/Y à H:i:s', $end_processing);
+            $processingdate = date_parse_from_format('d/m/Y à H:i:s', $endprocessing);
 
-            $processing_date = str_pad($processing_date["day"], 2, "0",STR_PAD_LEFT) . "/" . str_pad($processing_date["month"], 2, "0", STR_PAD_LEFT) . "/" . $processing_date["year"];
-        }
-        else {
-            $string_end_processing = REPORT_HYBRIDMETER_NA;
+            $processingdate = str_pad(
+                    $processingdate["day"],
+                    2,
+                    "0",
+                    STR_PAD_LEFT
+                ) . "/" .
+                str_pad(
+                    $processingdate["month"],
+                    2,
+                    "0",
+                    STR_PAD_LEFT
+                ) . "/" . $processingdate["year"];
+        } else {
+            $stringendprocessing = REPORT_HYBRIDMETER_NA;
         }
 
-        if($is_data_available && isset($processing_duration)){
-            $string_processing_duration = sprintf(
-                get_string('processing_duration','report_hybridmeter'),
-                $processing_duration
+        if ($isdataavailable && isset($processingduration)) {
+            $stringprocessingduration = sprintf(
+                get_string('processing_duration', 'report_hybridmeter'),
+                $processingduration
             );
-        }
-        else {
-            $string_processing_duration = REPORT_HYBRIDMETER_NA;
+        } else {
+            $stringprocessingduration = REPORT_HYBRIDMETER_NA;
         }
 
-        
-
-        
-        
-        $params=array(
+        $params = array(
             "title" => get_string('last_processing_results', 'report_hybridmeter'),
             "measurement_period_intro" => get_string('measurement_period_intro', 'report_hybridmeter'),
-            "measurement_period" => $string_measurement_period,
+            "measurement_period" => $stringmeasurementperiod,
             "measurement_disclaimer" => get_string('measurement_disclaimer', 'report_hybridmeter'),
-            "end_processing" => $string_end_processing,
-            "processing_date" => $processing_date,
-            "processing_duration" => $string_processing_duration,
+            "end_processing" => $stringendprocessing,
+            "processing_date" => $processingdate,
+            "processing_duration" => $stringprocessingduration,
             "name_columnname" => get_string('indicator_name', 'report_hybridmeter'),
             "value_columnname" => get_string('number', 'report_hybridmeter'),
             "name_digitalised_course" => get_string(REPORT_HYBRIDMETER_GENERAL_NB_DIGITALISED_COURSES, 'report_hybridmeter'),
-            "value_digitalised_course" => $nb_cours_hybrides_statiques,
+            "value_digitalised_course" => $nbcourshybridesstatiques,
             "name_used_course" => get_string(REPORT_HYBRIDMETER_GENERAL_NB_USED_COURSES, 'report_hybridmeter'),
-            "value_used_course" => $nb_cours_hybrides_dynamiques,
+            "value_used_course" => $nbcourshybridesdynamiques,
             "name_digitalisation_registered_students" => get_string(REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_DIGITALISED, 'report_hybridmeter'),
-            "value_digitalisation_registered_students" => $nb_etudiants_concernes_statiques,
+            "value_digitalisation_registered_students" => $nbetudiantsconcernesstatiques,
             "name_digitalisation_active_students" => get_string(REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_DIGITALISED_ACTIVE, 'report_hybridmeter'),
-            "value_digitalisation_active_students" => $nb_etudiants_concernes_statiques_actifs,
+            "value_digitalisation_active_students" => $nbetudiantsconcernesstatiquesactifs,
             "name_usage_registered_students" => get_string(REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_USED, 'report_hybridmeter'),
-            "value_usage_registered_students" => $nb_etudiants_concernes_dynamiques,
+            "value_usage_registered_students" => $nbetudiantsconcernesdynamiques,
             "name_usage_active_students" => get_string(REPORT_HYBRIDMETER_GENERAL_NB_STUDENTS_CONCERNED_USED_ACTIVE, 'report_hybridmeter'),
-            "value_usage_active_students" => $nb_etudiants_concernes_dynamiques_actifs,
+            "value_usage_active_students" => $nbetudiantsconcernesdynamiquesactifs,
             "name_nb_analysed_courses" => get_string(REPORT_HYBRIDMETER_GENERAL_NB_ANALYSED_COURSES, 'report_hybridmeter'),
-            "value_nb_analysed_courses" => $nb_cours_analyses,
+            "value_nb_analysed_courses" => $nbcoursanalyses,
         );
 
         $html = $OUTPUT->render_from_template("report_hybridmeter/indicators_table", $params);
 
-        $html .= html_writer::tag("hr","");
+        $html .= html_writer::tag("hr", "");
 
         return $html;
     }
