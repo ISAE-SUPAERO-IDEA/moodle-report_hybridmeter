3661beb2740910accf782f765355613696dcbe64
John Tranier <john.tranier@isae-supaero.fr>
2024-09-09 14:30:39 +0200

    Fix some code checker warnings

--- course.php
+++ course.php
@@ -1,4 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Display indicators for a specific course.
+ *
+ * @author Nassim Bennouar, John Tranier
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright (C) 2020  ISAE-SUPAERO (https://www.isae-supaero.fr/)
+ * @package report_hybridmeter
+ */
 
 require(dirname(__FILE__) . '/../../config.php');
 
@@ -15,8 +38,8 @@ $courseid = required_param('id', PARAM_INT);
 
 global $DB;
 
-$courseinfo = $DB->get_record('course', array("id" => $courseid));
-$data_provider = data_provider::get_instance();
+$courseinfo = $DB->get_record('course', ["id" => $courseid]);
+$dataprovider = data_provider::get_instance();
 
 $config = \report_hybridmeter\config::get_instance();
 $begin = $config->get_begin_date();
@@ -37,8 +60,8 @@ echo $output->course_indicators(
         indicators::nb_registered_students($courseid),
         indicators::active_students($courseid),
         indicators::is_course_active_on_period($courseid),
-        $data_provider->count_activities_per_type_of_course($courseid),
-        $data_provider->count_hits_on_activities_per_type(
+        $dataprovider->count_activities_per_type_of_course($courseid),
+        $dataprovider->count_hits_on_activities_per_type(
             $courseid,
             $begin,
             $end
